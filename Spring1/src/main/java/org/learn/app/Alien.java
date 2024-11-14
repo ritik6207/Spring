df8746882be8dc3ffc5f59c7796c040bc46acb65
Ritik Kumar <rk7382687@gmail.com>
2024-11-14 20:31:19 +0530

    Ref Attribute

--- Spring1/src/main/java/org/learn/app/Alien.java
+++ Spring1/src/main/java/org/learn/app/Alien.java
@@ -3,10 +3,12 @@ package org.learn.app;
 public class Alien {
 
     private int age;
+    private Laptop lap;
     public Alien(){
         System.out.println("Object created");
     }
     public void code(){
+        lap.compile();
         System.out.println("Codding...");
     }
 
@@ -17,5 +19,13 @@ public class Alien {
     public void setAge(int age) {
         System.out.println("Setter called");
         this.age = age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
     }
 }
--- Spring1/src/main/java/org/learn/app/Laptop.java
+++ Spring1/src/main/java/org/learn/app/Laptop.java
@@ -5,4 +5,8 @@ public class Laptop {
     public Laptop(){
         System.out.println("Laptop Object created...");
     }
+
+    public void compile(){
+        System.out.println("Compiling");
+    }
 }
