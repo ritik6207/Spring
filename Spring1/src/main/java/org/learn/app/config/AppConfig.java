f7d35f687ec552325f9e69a682eaac5b56b7ca2c
Ritik Kumar <rk7382687@gmail.com>
2024-11-19 19:43:27 +0530

    Primary&Qualifire Annotation

--- Spring1/src/main/java/org/learn/app/config/AppConfig.java
+++ Spring1/src/main/java/org/learn/app/config/AppConfig.java
@@ -3,16 +3,19 @@ package org.learn.app.config;
 import org.learn.app.Alien;
 import org.learn.app.Computer;
 import org.learn.app.Desktop;
+import org.learn.app.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public Alien alien(@Autowired Computer com){
+    public Alien alien( Computer com){ //@Qualifier("desktop")
         Alien obj = new Alien();
         obj.setAge(23);
         obj.setCom(com);
@@ -24,5 +27,11 @@ public class AppConfig {
 //    @Scope("prototype")
     public Desktop desktop(){
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop(){
+        return new Laptop();
     }
 }
