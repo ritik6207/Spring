c4cf9b101dc415a9c344360efe8119ae2f2990ea
Ritik Kumar <rk7382687@gmail.com>
2024-11-27 21:48:58 +0530

    displaying data on result page

--- SpringBootWeb/src/main/java/com/learn/SpringBootWeb/HomeController.java
+++ SpringBootWeb/src/main/java/com/learn/SpringBootWeb/HomeController.java
@@ -1,6 +1,7 @@
 package com.learn.SpringBootWeb;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,12 +13,14 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req){
+    public String add(HttpServletRequest req, HttpSession session){
 
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
-        System.out.println(result);
+
+        session.setAttribute("result",result);
+
         return "result.jsp";
     }
 }
