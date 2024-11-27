cf3eb61f60b7a04eadd4dbdc8a6aba04b613bf1c
Ritik Kumar <rk7382687@gmail.com>
2024-11-27 21:03:57 +0530

    Accepting Data the servlet way

--- SpringBootWeb/src/main/java/com/learn/SpringBootWeb/HomeController.java
+++ SpringBootWeb/src/main/java/com/learn/SpringBootWeb/HomeController.java
@@ -1,5 +1,6 @@
 package com.learn.SpringBootWeb;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,5 +9,15 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req){
+
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+        System.out.println(result);
+        return "result.jsp";
     }
 }
