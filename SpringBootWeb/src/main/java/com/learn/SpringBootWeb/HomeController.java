7e00ea493fa647560765168d1c3e9225437cca97
Ritik Kumar <rk7382687@gmail.com>
2024-11-27 22:08:42 +0530

    Model Object

--- SpringBootWeb/src/main/java/com/learn/SpringBootWeb/HomeController.java
+++ SpringBootWeb/src/main/java/com/learn/SpringBootWeb/HomeController.java
@@ -3,6 +3,7 @@ package com.learn.SpringBootWeb;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,12 +15,12 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num, @RequestParam("num2") int num2, HttpSession session){
+    public String add(@RequestParam("num1") int num, @RequestParam("num2") int num2, Model model){
 
 
         int result = num + num2 ;
 
-        session.setAttribute("result",result);
+        model.addAttribute("result",result);
 
         return "result.jsp";
     }
