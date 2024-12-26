0f3f3ffc780b43cd9138d018ea5e1bc36c6491ac
Ritik Kumar <rk7382687@gmail.com>
2024-12-26 19:30:55 +0530

    After Advice, AfterThrowing, AfterReturning

--- springAop/spring-boot-rest/src/main/java/com/learn/spring_boot_rest/aop/LoggingAspect.java
+++ springAop/spring-boot-rest/src/main/java/com/learn/spring_boot_rest/aop/LoggingAspect.java
@@ -2,8 +2,7 @@ package com.learn.spring_boot_rest.aop;
 
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -19,5 +18,20 @@ public class LoggingAspect {
     @Before("execution(* com.learn.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.learn.spring_boot_rest.service.JobService.updateJob(..))")
     public void logMethodCall(JoinPoint jp){
         LOGGER.info("Method call  " + jp.getSignature().getName());
+    }
+
+    @After("execution(* com.learn.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.learn.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodCallExecuted(JoinPoint jp){
+        LOGGER.info("Method Executed " + jp.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.learn.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.learn.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodCrashed(JoinPoint jp){
+        LOGGER.info("Method has some issue " + jp.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.learn.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.learn.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodCallExecutedSuccessfully(JoinPoint jp){
+        LOGGER.info("Method Executed successfully " + jp.getSignature().getName());
     }
 }
