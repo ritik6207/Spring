0bbd45639cab75318674534c4457cfb3d1184bd8
Ritik Kumar <rk7382687@gmail.com>
2024-12-26 19:03:47 +0530

    Before

--- springAop/spring-boot-rest/src/main/java/com/learn/spring_boot_rest/aop/LoggingAspect.java
+++ springAop/spring-boot-rest/src/main/java/com/learn/spring_boot_rest/aop/LoggingAspect.java
@@ -1,14 +1,22 @@
 package com.learn.spring_boot_rest.aop;
 
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
+
+//    return type, class-name.method-name(agrs)
+    @Before("execution(* com.learn.spring_boot_rest.service.JobService*(..)")
     public void logMethodCall(){
-        LOGGER.info("Log Method call");
+        LOGGER.info("Method call");
     }
 }
