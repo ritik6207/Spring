1cd9ee75ddd4ce06d6e63614fa00d77a23edce2d
Ritik Kumar <rk7382687@gmail.com>
2024-12-27 21:26:41 +0530

    working with multiple users

--- spring-security/src/main/java/com/learn/spring_security/config/SecurityConfig.java
+++ spring-security/src/main/java/com/learn/spring_security/config/SecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,4 +34,24 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("ritik")
+                .password("r@123")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin@123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
