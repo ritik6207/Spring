b3257189c0d4ed74e5e13e5e1cd4659e4a375cd0
Ritik Kumar <rk7382687@gmail.com>
2024-12-27 22:43:22 +0530

    Creating UserDetailsService

--- spring-security/src/main/java/com/learn/spring_security/controller/HelloController.java
+++ spring-security/src/main/java/com/learn/spring_security/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.learn.spring_security;
+package com.learn.spring_security.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
