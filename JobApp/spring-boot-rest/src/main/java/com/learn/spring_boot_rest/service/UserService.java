af16c4963080d3ec72cc2e1016b5757fc4707a52
Ritik Kumar <rk7382687@gmail.com>
2024-12-28 22:54:55 +0530

    Securing Job App

---
+++ JobApp/spring-boot-rest/src/main/java/com/learn/spring_boot_rest/service/UserService.java
@@ -0,0 +1,18 @@
+package com.learn.spring_boot_rest.service;
+
+
+import com.learn.spring_boot_rest.model.User;
+import com.learn.spring_boot_rest.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    public User saveUser(User user){
+        return repo.save(user);
+    }
+}
