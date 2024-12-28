44995dd4466f201869f93bb2b7e17eed5cf6fec5
Ritik Kumar <rk7382687@gmail.com>
2024-12-28 22:39:58 +0530

    m

---
+++ JobApp/spring-boot-rest/src/main/java/com/learn/spring_boot_rest/repo/JobRepo.java
@@ -0,0 +1,15 @@
+package com.learn.spring_boot_rest.repo;
+
+import com.learn.spring_boot_rest.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer> {
+
+    List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+
+}
